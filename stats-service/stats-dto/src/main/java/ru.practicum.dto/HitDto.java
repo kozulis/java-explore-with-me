836d760c7f6d7716bb0f1404cb81977d511688de
Milovanov Aleksey <kozulis@yandex.ru>
2023-07-23 23:22:06 +0300

    fix: победоносная борьба с ошибками v4

--- stats-service/stats-dto/src/main/java/ru.practicum.dto/HitDto.java
+++ stats-service/stats-dto/src/main/java/ru.practicum.dto/HitDto.java
@@ -1,13 +1,11 @@
 package ru.practicum.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
-import java.time.LocalDateTime;
 
 @Data
 @Builder
